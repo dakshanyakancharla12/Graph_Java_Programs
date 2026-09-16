f534ce1603b869db0c83f704938f55e9e2c55775
Kancharla Dakshanya <dakshanyakancharla08@gmail.com>
2026-09-16 14:16:34 +0530

    DFSStack

--- src/Demo/DFSStack.java
+++ src/Demo/DFSStack.java
@@ -21,7 +21,41 @@ public class DFSStack {
 				continue;
 			}
 			
+			visited[current] = true;
 			
+			System.out.print(current + " ");
+			
+			for (int neighbour : graph.get(current)) {
+				if (!visited[neighbour]) {
+					stack.push(neighbour);
+				}
+			}
 		}
+	}
+	
+	public static void main(String[] args) {
+		int vertices = 6;
+		
+		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+		
+		for (int i = 0; i < vertices; i++) {
+			graph.add(new ArrayList<>());
+		}
+		
+		addEdge(graph, 0, 1);
+		addEdge(graph, 0, 2);
+		addEdge(graph, 1, 3);
+		addEdge(graph, 1, 4);
+		addEdge(graph, 2, 5);
+		
+		dfs(graph, 0);
+	}
+	
+	static void addEdge(
+			ArrayList<ArrayList<Integer>> graph,
+			int u,
+			int v) {
+		graph.get(u).add(v);
+		graph.get(v).add(u);
 	}
 }
